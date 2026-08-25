4f03de49aec78da7f76d2e1141b3b3ab55dcdf41
EMMANUEL-KEVOGO <105873372+EMMANUEL-KEVOGO@users.noreply.github.com>
2026-08-25 23:55:40 +0300

    Add Terraform variables for AWS configuration

---
+++ .github/workflows/app/terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "environment" {
+  type    = string
+  default = "production"
+}
+
+variable "app_name" {
+  type    = string
+  default = "ekyc-serverless-api"
+}
